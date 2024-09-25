f870ea4e1bacce93fa1b1ec439fd2914e7e2aeab
borrowedbirch05 <borrowedbirch05@gmail.com>
2024-09-25 11:59:19 -0500

    use ollama instead of chatgpt

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 if __name__ == "__main__":
     load_dotenv()
@@ -25,7 +26,8 @@ In October 2002, eBay acquired PayPal for $1.5 billion, and that same year, with
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOllama(model="llama3.1")
 
     chain = summary_prompt_template | llm
     res = chain.invoke(input={"information": information})
