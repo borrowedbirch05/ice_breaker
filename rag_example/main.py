42b566835699cba218f3d7abe50fdaadd7ced67d
borrowedbirch05 <borrowedbirch05@gmail.com>
2024-11-07 10:49:10 -0600

    create documentation helper using streamlit

--- rag_example/main.py
+++ rag_example/main.py
@@ -7,7 +7,13 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
+from langchain_core.runnables import RunnablePassthrough
+
 load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print("retrieving")
@@ -17,7 +23,7 @@ if __name__ == "__main__":
 
     #What happens if we dont use RAG. ie dont query vector store for context
 
-    query = "what is pinecone in machine learning?"
+    query = "what is blah"
     # chain = PromptTemplate.from_template(template=query) | llm
     # result = chain.invoke(input={})
     # print(result.content)
@@ -42,14 +48,39 @@ if __name__ == "__main__":
         Basically, this will take the query, get embeddings, get relevant embeddings 
         and pass everything to llm and get a response
     """
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    retrieval_chain = create_retrieval_chain(
-        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+
+    # combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    # retrieval_chain = create_retrieval_chain(
+    #     retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    # )
+
+    # result = retrieval_chain.invoke(input={"input": query})
+
+    # print(result['answer'])
+
+
+
+    template = """
+            Use the following context to answer the question at the end. If you dont know the answer, 
+            just say "u dumb", dont make up the answer. Be as concise as possible.
+
+            {context}
+
+            Question: {question} 
+
+            Answer:
+    """
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context" : vectorstore.as_retriever() | format_docs, "question" : RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
     )
 
-    result = retrieval_chain.invoke(input={"input": query})
-
-    print(result['answer'])
+    res = rag_chain.invoke(query)
+    print(res)
 
 
 
